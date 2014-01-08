8178e1e4706be20f8f5ff0682a6153168f9946be
Tyler Spivey <tspivey@pcdesk.net>
2014-01-08 01:09:37 -0800

    Find resources in the right place when not frozen

--- resource_finder.py
+++ resource_finder.py
@@ -9,7 +9,7 @@ def find_application_resource(resource_name, extra_path=''):
  if is_frozen():
   base_path = embedded_data_path()
  else:
-  base_path = module_path()
+  base_path = module_path(3)
  path = os.path.join(base_path, extra_path, resource_name)
  if not os.path.exists(path):
   raise ResourceNotFoundError("Could not find application resource %r" % path)
