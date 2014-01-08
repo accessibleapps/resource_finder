97554da8d1353274e33b158bbb3537f05a343275
Q <q@q-continuum.net>
2014-01-08 02:00:54 -0700

    Only join the extra_path if it's not a string.

--- resource_finder.py
+++ resource_finder.py
@@ -4,12 +4,13 @@ from platform_utils.paths import module_path, is_frozen, embedded_data_path
 class ResourceNotFoundError(IOError): pass
 
 def find_application_resource(resource_name, extra_path=''):
- extra_path = os.path.join(extra_path)
+ if not isinstance(extra_path, basestring):
+  extra_path = os.path.join(*extra_path)
  if is_frozen():
   base_path = embedded_data_path()
  else:
   base_path = module_path()
  path = os.path.join(base_path, extra_path, resource_name)
  if not os.path.exists(path):
-  raise ResourceNotFoundError("Could not find application resource %r" % resource_name)
+  raise ResourceNotFoundError("Could not find application resource %r" % path)
  return path
